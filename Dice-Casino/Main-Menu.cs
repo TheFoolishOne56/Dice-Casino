9db6dc1e1667d903a80f59665c4c48ab45889e34
TheFoolishOne56 <Whitacre5656@gmail.com>
2025-02-26 06:51:40 -0500

    good luck

--- Dice-Casino/Main-Menu.cs
+++ Dice-Casino/Main-Menu.cs
@@ -10,7 +10,14 @@ public class MainMenu
         var Game = Console.ReadLine();
         if Game == "Yootz"||"yootz";
         {
+            // ## if you want to bring back the function just get rid of the //.
+            //Yootzgame
             break
         }
     }
+    // ## Works as a function when placed in Main-Menu.cs but not when in Yootz.cs. 
+    //public static void Yootzgame();
+    //{
+    //    Console.WriteLine("Welcome to Yootz");
+    //}
 }
--- Dice-Casino/Yootz.cs
+++ Dice-Casino/Yootz.cs
@@ -1,9 +1,42 @@
 namespace DiceCasino;
 
-public class Yootz
+public static void Yootzgame()
 {
-    public static void Yootzgame()
+    Playedbefore:
+    Console.WriteLine("Welcome to Yootz! Have you played before? (yes/no)");
+    var hasPlayed = Console.ReadLine();
+    // ## THE FUCKING IF STATMENT WONT TURN PURPLE  TO SHOW THAT IT IS ACTIVATED BUT THE ELSE IF AND ELSE STATMENT WORK GREAT!!!! FUUUUUUCK
+    //## for the 
+    if hasPlayed == "yes"||"Yes";
     {
-        Console.WriteLine("Welcome to Yootz");
+        GamePlay//these are functions that are not being called
     }
+    else if hasPlayed == "no"||"No";
+    {
+        GameRules//these are functions that are not being called
+    }
+    else
+    {
+        Console.WriteLine("That was not a valid answer. Please type yes/Yes or no/No");
+        goto Playedbefore;
+    }
+}
+
+public static void GameRules()
+{//#this is writing out the rules before using the GamePlay function.
+    Console.WriteLine("Starting off we have the basics of the gameplay which is how to roll the dice. \n" 
+    "Rolling the Dice: Each player takes a turn rolling the five dice. \n"+ 
+    "Players can roll the dice up to three times per turn to get the highest-scoring combination. \n"
+    "First Roll: The player rolls all five dice. \n"
+    "Second and Third Rolls: After the first roll, the player can choose to keep any dice they want and re-roll the rest. \n"
+    "They may re-roll some or all of the dice two more times. \n"
+    "Players can also choose to stop after the first or second roll if they have a combination they like. \n"
+    "Scoring: After rolling, the player chooses a category to score their dice. \n"
+    "Each category can only be used once per game. Categories are divided into two sections: Upper Section and Lower Section.");
+    Console.WriteLine()//Continuing the ruls in the two different sections.
+}
+
+public static void GamePlay()
+{
+    break;
 }
